c746735631b09b5091f3668754b4bd4769182a96
Joseph Ma <55027668+ma562@users.noreply.github.com>
2023-01-31 14:04:04 -0500

    Create injection2.c

---
+++ lab2/apps/q5/makeprocs/injection2.c
@@ -0,0 +1,30 @@
+#include "lab2-api.h"
+#include "usertraps.h"
+#include "misc.h"
+
+#include "spawn.h"
+
+void main (int argc, char *argv[])
+{
+  sem_t s_procs_completed; // Semaphore to signal the original process that we're done
+
+  if (argc != 4) { 
+    Printf("Usage: "); Printf(argv[0]); Printf(" <handle_to_shared_memory_page> <handle_to_page_mapped_semaphore>\n"); 
+    Exit();
+  } 
+  
+  // Convert the command-line strings into integers for use as handles
+  sem_t h2o = dstrtol(argv[1], NULL, 10); // The "10" means base 10
+  s_procs_completed = dstrtol(argv[2], NULL, 10);
+
+  for(int i = 0; i < argv[3]; i++) {
+    sem_signal(h2o); 
+  }
+
+  // Signal the semaphore to tell the original process that we're done
+  Printf("spawn_me: PID %d is complete.\n", Getpid());
+  if(sem_signal(s_procs_completed) != SYNC_SUCCESS) {
+    Printf("Bad semaphore s_procs_completed (%d) in ", s_procs_completed); Printf(argv[0]); Printf(", exiting...\n");
+    Exit();
+  }
+}
